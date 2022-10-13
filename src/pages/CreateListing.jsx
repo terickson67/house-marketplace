cf6c32637f21d651cf17201455848be14fa42a3e
Taylor Erickson <terickson@cyberhusky.io>
2022-10-13 09:47:43 -0400

    geolcation and env file

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,6 +1,7 @@
 import {useState, useEffect, useRef} from 'react'
 import {getAuth, onAuthStateChanged} from 'firebase/auth'
 import {useNavigate} from 'react-router-dom'
+import {toast} from 'react-toastify'
 import Spinner from '../components/Spinner'
 
 function CreateListing() {
@@ -59,9 +60,57 @@ function CreateListing() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isMounted])
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault()
-  }
+
+    setLoading(true)
+
+    if(discountedPrice >= regularPrice){
+        setLoading(false)
+        toast.error('Discounted Price needs to be less than Regular Price')
+        return
+    }
+
+    if(images.length > 6){
+        setLoading(false)
+        toast.error('Max 6 images')
+        return
+    }
+
+    let geolocation = {}
+    let location
+
+    if(geolocationEnabled){
+        const response = await fetch (`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
+        )
+
+
+        console.log(process.env.REACT_APP_GEOCODE_API_KEY)
+
+        const data = await response.json()
+
+        geolocation.lat = data.results[0]?.geometry.location.lat ?? 0
+        geolocation.lng = data.results[0]?.geometry.location.lng ?? 0
+
+        console.log('data.status: ', data.status)
+
+        location = data.status === 'ZERO_RESULTS' 
+            ? undefined 
+            : data.results[0]?.formatted_address
+
+        if(location === undefined || location.includes('undefined')){
+            setLoading(false)
+            toast.error('Please enter a valid address')
+            return
+        }
+    }else{
+        geolocation.lat = latitude
+        geolocation.lng = longitude
+        location = address 
+    }
+
+    setLoading(false)
+}
 
   const onMutate = (e) => {
     let boolean = null
